97210e936d9a005ebc303372e00c13e3d6b1479a
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 00:15:51 +0300

    HBNB Project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,16 @@
--- prepares a MySQL server for the project
+-- preparing the MySQL server
 
+-- Create the database if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it does not already exist and set the password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the hbnb_dev_db database to the user
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- Grant select privileges on the performance_schema to the user
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- Apply the changes
 FLUSH PRIVILEGES;
