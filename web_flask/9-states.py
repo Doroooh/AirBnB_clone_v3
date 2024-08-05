265785073f52e1e66691a72ca4106949c3bb62f2
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 00:41:48 +0300

    HBNB Project

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Sets up a Flask web server
 """
 
 from flask import Flask, render_template
 from models import *
 from models import storage
+
 app = Flask(__name__)
 
-
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
-
+@app.route('/all_states', strict_slashes=False)
+@app.route('/all_states/<state_id>', strict_slashes=False)
+def show_states(state_id=None):
+    """Displays states and their cities in alphabetical order, or a specific state if an ID is provided"""
+    all_states = storage.all("State")
+    if state_id:
+        state_id = f'State.{state_id}'
+    return render_template('9-states.html', states=all_states, state_id=state_id)
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close_storage(exception):
+    """Closes the storage session when the application context ends"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
