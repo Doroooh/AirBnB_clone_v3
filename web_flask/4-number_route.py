83aaeded441a5c6f3f3ef5f29d8c4be949bab603
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 00:35:35 +0300

    HBNB Project

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" Set up Flask web server """
 
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def home():
+    """Respond with 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
+def display_hbnb():
+    """Respond with 'HBNB'"""
     return 'HBNB'
 
-
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+def display_c_text(text):
+    """Shows 'C ' followed by the provided text with underscores replaced by spaces"""
     return 'C ' + text.replace('_', ' ')
-
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+def python_text(text='is cool'):
+    """Display 'Python ' followed by provided text, defaulting to 'is cool'"""
     return 'Python ' + text.replace('_', ' ')
 
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-    """display “n is a number” only if n is an integer"""
-    return "{:d} is a number".format(n)
-
+@app.route('/number/<int:num>', strict_slashes=False)
+def number_display(num):
+    """Displays 'num is a number' only if num is an integer"""
+    return f"{num} is a number"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
