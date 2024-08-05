659dd3261e78ac2770688dd1f4c35d8e49bd0a02
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 00:31:27 +0300

    HBNB Project

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" Initialize the Flask web service """
 
 from flask import Flask, render_template
 from models import *
 from models import storage
+
 app = Flask(__name__)
 
-
-@app.route('/hbnb_filters', strict_slashes=False)
-def filters():
-    """display a HTML page like 6-index.html from static"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
-
+@app.route('/filters_page', strict_slashes=False)
+def show_filters():
+    """Render an HTML page similar to 6-index.html from static"""
+    state_objects = storage.all("State").values()
+    amenity_objects = storage.all("Amenity").values()
+    return render_template('10-hbnb_filters.html', states=state_objects,
+                           amenities=amenity_objects)
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close_storage(exception):
+    """Closes the storage connection when tearing down"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
