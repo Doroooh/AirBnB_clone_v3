a0b4389ed7f7942afb1976b69d3868b86ee3437d
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 00:32:25 +0300

    HBNB Project

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" Launch the Flask-based web service """
 
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def home():
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
+def show_hbnb():
+    """Displays 'HBNB'"""
     return 'HBNB'
 
-
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+def display_text(text):
+    """Shows 'C ' followed by the provided text, with underscores replaced by spaces"""
     return 'C ' + text.replace('_', ' ')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
