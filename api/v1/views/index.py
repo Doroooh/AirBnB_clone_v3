a5cf75ef4a549c36e497d4354f6b91805dbd26ca
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 00:09:39 +0300

    HBNB Project
    HBNB Project

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3xx
-'''api status'''
+""" api status """
 import models
 from models import storage
 from models.base_model import BaseModel
@@ -9,13 +9,12 @@ from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def returnstuff():
-    '''return stuff'''
+   """ to return """
     return jsonify(status='OK')
-
 
 @app_views.route('/stats', strict_slashes=False)
 def stuff():
-    '''JSON Responses'''
+    """ TheJSON Response """
     todos = {'states': State, 'users': User,
             'amenities': Amenity, 'cities': City,
             'places': Place, 'reviews': Review}
