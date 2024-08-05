e3f17ceb5bb3e4a535eb40006f161fa859d5de88
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 00:33:57 +0300

    HBNB Project

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" Initialize  Flask web server """
 
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def home_page():
+    """Return 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
+def hbnb_page():
+    """Return string 'HBNB'"""
     return 'HBNB'
 
-
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+def display_c(text):
+    """Shows 'C ' followed by text parameter with underscores replaced with spaces"""
     return 'C ' + text.replace('_', ' ')
-
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+def python_message(text='is cool'):
+    """Displays 'Python ' followed by the text parameter with underscores replaced with spaces"""
     return 'Python ' + text.replace('_', ' ')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
