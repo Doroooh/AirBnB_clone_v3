a30e74c0086ae1661fb7752da5b2bddd4e05f2ee
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 00:40:43 +0300

    HBNB Project

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" Initialize Flask web server """
 
 from flask import Flask, render_template
 from models import *
 from models import storage
+
 app = Flask(__name__)
 
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
-
+@app.route('/state_cities', strict_slashes=False)
+def state_cities():
+    """Renders an HTML page with states and their cities sorted alphabetically"""
+    all_states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=all_states)
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close_storage_session(exception):
+    """Close the storage session when the application context is torn down"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
