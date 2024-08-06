7b4ac726d724b395d35d721016d4a03e39246e85
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-06 23:28:04 +0300

    HBNB Project

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,53 @@
 #!/usr/bin/python3
-""" The Flask server """
-
-from flask import Flask, jsonify
-from models import storage
+"""
+Module containing Flask API
+"""
 from os import getenv
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+
+from models import storage
 from api.v1.views import app_views
 
+# Initiate the Flask app
 app = Flask(__name__)
+
+
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
+
+# HTTP access control (CORS) CORS instance allowing:
+# CORS instance allowing: /* for 0.0.0.0
+# [0.0.0.0](any origin is allowed)
+# you can see this HTTP Response Header: < Access-Control-Allow-Origin: 0.0.0.0
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def downtear(self):
-    """ The status of the API """
+def close_db(obj):
+    """Method that removes current session"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """ to return render_template """
-    return jsonify('error='Not found'), 404
+    """Method that handles 404 status in JSON format"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+# Configure Swagger (OpenAPI) documentation `http://127.0.0.1:5000/apidocs/`
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone - RESTful API',
+    'description': 'This is the api that was \
+        created for the hbnb restful api project,\
+    all the documentation will be shown below',
+    'uiversion': 3}
+
+Swagger(app)
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host, int(port), threaded=True)
